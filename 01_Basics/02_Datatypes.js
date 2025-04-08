877cd8341617c62bffc53c8cada6baae59c22f3b
git-sahil36 <sahilpriyadarshivpn@gmail.com>
2025-04-08 05:16:56 +0000

    New File Added

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,15 @@
+'use strict'  //trats all JS  codee as per newer standards
+
+// alert (3+3)
+
+console.log(3+3) //code readibility should be very high and uunnecessary things dhould be advoided.
+
+console.log('Sahil_36')
+
+let name = 'Sahil_36'
+let age = 19
+let isLoggedIn = false
+
+console.log(name)
+console.log(age)
+console.log(isLoggedIn)
