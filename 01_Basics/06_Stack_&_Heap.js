3aad8fc20379ab12577d7ce927c0c7033b0af0ef
git-sahil36 <sahilpriyadarshivpn@gmail.com>
2025-04-09 07:12:59 +0000

    New Files Added

---
+++ 01_Basics/06_Stack_&_Heap.js
@@ -0,0 +1,16 @@
+let name1 = 'Sahil'
+
+let name2 = name1
+name2 = 'Ji'
+
+console.log(name1)
+console.log(name2)
+
+let user1 = {
+    email : 'user@gmail.com',
+    upi : 'user@sbi'
+}
+
+let user2 = user1
+
+console.log(user2)
