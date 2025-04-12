7c83517eef9950e93d7adf6928740b31580b6ad4
git-sahil36 <sahilpriyadarshivpn@gmail.com>
2025-04-12 05:01:31 +0000

    New Files Added

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,15 @@
+// Arrreeeee....
+
+const arr1 = [0,1,2,3,4,5]
+
+const Heros = ['Mom','Dad','Family']
+
+const arr2 = new Array(1,2,3,4)
+console.log(arr1[2]);
+console.log(Heros);
+
+// Methods in Arrays
+
+arr2.push(6)
+arr2.push(7)
+console.log(arr2);
