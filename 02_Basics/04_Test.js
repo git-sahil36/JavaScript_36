46b5a747b5845fef9d45be04918d6698860732e7
git-sahil36 <sahilpriyadarshivpn@gmail.com>
2025-06-23 19:06:37 +0530

    Test_File_Added

---
+++ 02_Basics/04_Test.js
@@ -0,0 +1,7 @@
+const arr1 = [0,1,2,3,4,5]
+
+const Heros = ['Mom','Dad','Family','& Most Importantly I , Me , aur Main']
+
+const arr2 = new Array(1,2,3,4)
+console.log(arr1[2]);
+console.log(Heros);
